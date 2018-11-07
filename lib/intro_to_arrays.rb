afb20d2643b8ec708d534222ce931e82c1c5269c
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-11-07 03:23:52 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,3 +7,15 @@ array_with_two_elements
   array = ["hello", "moto"]
   array
 end
+
+first_element(array)
+  array[0]
+end
+
+third_element(array)
+  array[2]
+end
+
+last_element(array)
+  array[-1]
+end
